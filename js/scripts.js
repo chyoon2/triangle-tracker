d3b3f06bf3841b4f7c2059dad3ea42627dba82b6
Chris Yoon <chy.yoon@gmail.com>
2020-06-04 14:51:28 -0700

    Add html labels and outputs as well as business and user interface logic
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,41 @@
-//Businasty logic
+// //Businasty logic
+let triangle;
+if (parseInt($("input#side-a").val()) + parseInt($("input#side-b").val()) === parseInt($("input#side-c").val()) || parseInt($("input#side-b").val()) + parseInt($("input#side-c").val()) === parseInt($("input#side-a").val()) || praseInt($("input#side-a").val()) + parseInt($("input#side-c").val())) {
+  triangle = "Not a triangle"
+}
+else {
+  if (parseInt($("input#side-a").val()) === parseInt($("input#side-b").val()) && parseInt($("input#side-b").val()) === parseInt($("input#side-c").val())) {
+    triangle = "Equilateral"
+  }
+  else if (parseInt($("input#side-a").val()) === parseInt($("input#side-b").val()) || parseInt($("input#side-b").val()) === parseInt($("input#side-c").val())) {
+    triangle = "Isosceles"
+  }
+  else if (parseInt($("input#side-a").val()) != parseInt($("input#side-b").val()) && parseInt($("input#side-a").val()) != parseInt($("input#side-c").val()) && parseInt($("input#side-b").val()) != parseInt($("input#side-c").val())) {
+    triangle = "Scalene"
+  }
+}
 
 //Interface logic
 $(document).ready(function() {
   $("#form-one").submit(function(event) {
     event.preventDefault();
-    const 
 
-    if
+    if (triangle==="Scalene"){
+      (".output").show();
+      ("#scalene").show();
+    }
+    else if (triangle==="Isosceles"){
+      (".output").show();
+      ("#isosceles").show();
+    }
+    else if (triangle==="equilateral"){
+      (".output").show();
+      ("#scalene").show();
+    }
+    else {
+      (".output").show();
+      ("#not-triangle").show;
+    }
+
   });
 });
